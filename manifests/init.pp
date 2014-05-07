df7d9a78e2ef25bd966e91af52c182670f716bfc
belimor <dmitry.vagin@cybera.ca>
2014-05-07 12:16:35 -0600

    updated init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class rattic {
 
-	include rattic::apache ->
-	include rattic::packages ->
-	include rattic::mysql ->
-	include rattic::rattic_setup 
+	class {'rattic::apache': } ->
+	class {'rattic::packages':} ->
+	class {'rattic::mysql':} ->
+	class {'rattic::rattic_setup':} 
 	
 
 }
