87091275bf5b0b9c5ad859dbed66e0530953cda5
Dmitry <dmitry.vaghin@cybera.ca>
2015-10-27 20:25:26 +0000

    formatting improvements. added version variable for wget command

--- manifests/rattic_setup.pp
+++ manifests/rattic_setup.pp
@@ -1,23 +1,26 @@
 class rattic::rattic_setup {
   
-  $version = "1.0.1"
+  $version = "1.2.2"
 
 ####################################################################################
 ### rattic
 ####################################################################################
+
   file { '/opt/apps' :
     ensure  => 'directory',
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
   }
+
   exec { 'wget' :
-    command => 'wget https://github.com/tildaslash/RatticWeb/archive/v1.0.1.tar.gz',
+    command => 'wget https://github.com/tildaslash/RatticWeb/archive/v{version}.tar.gz',
     cwd     => '/opt/apps',
     creates => "/opt/apps/v${version}.tar.gz",
     path    => "/usr/bin/",
     require => File['/opt/apps'],
   }
+
   exec { 'extract' :
     command => "tar -zxvf v${version}.tar.gz",
     cwd     => '/opt/apps',
@@ -25,12 +28,14 @@ class rattic::rattic_setup {
     path    => "/usr/local/bin/:/bin/",
     require => Exec['wget'],
   }
+
   exec { "rename":
     command => "mv /opt/apps/RatticWeb-${version} /opt/apps/RatticWeb",
     creates => "/opt/apps/RatticWeb",
     path    => "/usr/local/bin/:/bin/",
     require => Exec['extract'],
   }
+
   file { '/opt/apps/RatticWeb/static' :
     ensure  => 'directory',
     owner   => 'root',
@@ -38,6 +43,7 @@ class rattic::rattic_setup {
     mode    => '0775',
     require => Exec['rename'],
   }
+
   file { '/opt/apps/RatticWeb/conf/local.cfg' :
     ensure  => 'file',
     owner   => 'root',
@@ -46,6 +52,7 @@ class rattic::rattic_setup {
     source  => 'puppet:///modules/rattic/local.cfg',
     require  => Exec['rename'],
   }
+
   file { '/opt/apps/rattic.script' :
     ensure  => 'file',
     owner   => 'root',
@@ -54,6 +61,7 @@ class rattic::rattic_setup {
     source  => 'puppet:///modules/rattic/rattic_script',
     require => File ['/opt/apps'],
   }
+
   exec { "initial_setup":
     command => "/opt/apps/rattic.script",
     require => File ['/opt/apps/RatticWeb/conf/local.cfg', '/opt/apps/rattic.script'],
